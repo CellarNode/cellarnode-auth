731bf7127a26f8aebe87b5d7ab6ebec080de1022
mjnong <marcusnagy@icloud.com>
2026-03-14 23:55:48 +0100

    feat: finalize React barrel export with all components, bump to v0.2.0

--- src/react/index.ts
+++ src/react/index.ts
@@ -1,3 +1,12 @@
+"use client";
+
+export { LoginForm, type LoginFormProps } from "./login-form.js";
+export { RegisterForm, type RegisterFormProps } from "./register-form.js";
+export {
+  UnauthorizedPage,
+  type UnauthorizedPageProps,
+} from "./unauthorized.js";
+export { SquircleShift, type SquircleShiftProps } from "./squircle-shift.js";
 export {
   InputOTP,
   InputOTPGroup,
@@ -5,12 +14,3 @@ export {
   InputOTPSeparator,
   type InputOTPProps,
 } from "./input-otp-slots.js";
-
-export { LoginForm, type LoginFormProps } from "./login-form.js";
-
-export { RegisterForm, type RegisterFormProps } from "./register-form.js";
-
-export {
-  UnauthorizedPage,
-  type UnauthorizedPageProps,
-} from "./unauthorized.js";
