eb55fa3412665340c4f8faf449ba98f7a49a5aa1
mjnong <marcusnagy@icloud.com>
2026-03-14 23:39:16 +0100

    feat: add core barrel export

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+export { createAuthStore } from "./auth-store.js";
+export { createAuthClient } from "./auth-client.js";
+export { createAuthApi } from "./auth-api.js";
+export { validateUserType } from "./auth-guard.js";
+export { extractAccessToken } from "./extract-token.js";
+
+export {
+  AuthError,
+  type AuthUser,
+  type AuthStore,
+  type AuthStoreConfig,
+  type AuthClient,
+  type AuthClientConfig,
+  type AuthApi,
+  type RegisterInput,
+  type RequestOtpResponse,
+  type VerifyOtpResponse,
+  type AuthErrorResponse,
+  type UserType,
+} from "./types.js";
---
+++ src/react/index.ts
@@ -0,0 +1,3 @@
+// React components will be added in a future task.
+// This barrel export satisfies the package.json exports map.
+export {};
